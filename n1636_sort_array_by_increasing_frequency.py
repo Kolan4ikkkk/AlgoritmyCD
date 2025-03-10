179e20776ec427af52818669170da3aa58bfd339
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2025-03-10 15:38:20 +0300

    Solve

---
+++ n1636_sort_array_by_increasing_frequency.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def frequencySort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        from collections import Counter
+        
+        freq = Counter(nums)
+        
+        nums.sort(key=lambda x: (freq[x], -x))
+        
+        return nums
